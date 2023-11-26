9b53ed114233e5b2b24d4ad4775ddb1a5bcf8c4f
Theo Zhang <azhang8@g.ucla.edu>
2023-11-25 16:16:05 -0800

    Removed ROC and AUC
    Basic threshold worked better than optimized threshold

--- pytorch/evaluate.py
+++ pytorch/evaluate.py
@@ -33,77 +33,19 @@ class Evaluator(object):
         clipwise_output = output_dict['clipwise_output']   # (audios_num, classes_num)
         # label from dataset
         target = output_dict['target']    # (audios_num, classes_num)
-        print("target dimension:", target.shape)
+        # print("target dimension:", target.shape)
         # accuracy = calculate_accuracy(target, clipwise_output)
-
         # convert the predicted labels into probabilities
-        # prob_clipwise_output = np.exp(clipwise_output) # might use sigmoid?
-        # threshold = 0.5
-        # prob_clipwise_output[prob_clipwise_output > threshold] = 1
-        # prob_clipwise_output[prob_clipwise_output <= threshold] = 0
+        prob_clipwise_output = np.exp(clipwise_output) # might use sigmoid?
+        threshold = 0.5
+        prob_clipwise_output[prob_clipwise_output > threshold] = 1
+        prob_clipwise_output[prob_clipwise_output <= threshold] = 0
 
-        # f1 = f1_score(target, prob_clipwise_output, average = "weighted")
-        
-        # ////// ROC CURVE PART //////
-        # for each label (i), compute the roc_curve
-        class_metrics = {}
-        prob_clipwise_output = 1/(1 + np.exp(-clipwise_output)) # sigmoid 
-        print("prob_clipwise_output:", prob_clipwise_output)
-        predicted_output = np.zeros_like(prob_clipwise_output)
-        for i in range(self.num_classes):
-          fpr, tpr, thresholds = roc_curve(target[:, i], prob_clipwise_output[:, i])
-          # how confident the model is for being right; range: 
-          auc = roc_auc_score(target[:, i], prob_clipwise_output[:, i])
-        
-          youden_j_stat = tpr - fpr
-          print("tpr:", tpr)
-          print("fpr:", fpr)
-          print("youden_j_stat:", youden_j_stat, "w/ dimensions", youden_j_stat.shape)
-
-          optimal_threshold = thresholds[np.argmax(youden_j_stat)]
-          print(f"optimal threshold for class {i}: {optimal_threshold} at index {np.argmax(youden_j_stat)}")
-          # predicted_output[:, i] = 1 if prob_clipwise_output[:, i] >= optimal_threshold else 0
-          predicted_output = np.where(prob_clipwise_output >= optimal_threshold, 1, 0)
-          class_metrics[i] = {
-              "fpr" : fpr,
-              "tpr" : tpr,
-              "auc" : auc,
-              "threshold" : optimal_threshold
-          }
-        print("predicted_output (after loop):", predicted_output)
-        self.plot_roc(class_metrics, self.num_classes)
-
-  
-        self.plot_cm(target, predicted_output, self.num_classes)
-
-        # TO DO: f1 based on 0.5 vs optimal threshold
-        f1 = f1_score(target, predicted_output, average = "weighted")
+        f1 = f1_score(target, prob_clipwise_output, average = "weighted")
         statistics = {'f1': f1}
         self.png_counter += 5
 
         return statistics
-    
-    def plot_roc(self, classes_metrics, num_classes):
-      classes = ["Automotive", "Food & Drink", "Pets", "War & Conflicts", "Music"]
-      f, axes = plt.subplots(1, num_classes, figsize=(15, 4))
-      axes = axes.ravel()
-
-      for i in range(num_classes):
-        metric = classes_metrics[i]
-        axes[i].plot(metric["fpr"], metric["tpr"], label=f'Class {i + 1} (AUC = {metric["auc"]:.2f}, Threshold = {metric["threshold"]:.2f})')
-        axes[i].set_title(f'{classes[i]}', fontsize = 8)
-        if i < 10:
-            axes[i].set_xlabel('')
-        if i % 5 != 0:
-            axes[i].set_ylabel('')
-        axes[i].legend(loc = "lower right", bbox_to_anchor=(1.0, 0.0), fontsize= 6)
-
-      plt.subplots_adjust(wspace=0.50, hspace=0.1)
-
-      filename = f"epoch_roc_{self.png_counter}.png"
-      filepath = os.path.join("/content/figures/", filename) 
-      
-      plt.savefig(filepath)
 
     def plot_cm(self, dataset_label, model_prediction, num_classes):
        
