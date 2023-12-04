25e13e600a5a5975521b2dffce36d4aa3fb9459c
Megan Chung <80943637+meganchung4444@users.noreply.github.com>
2023-12-04 00:15:59 -0800

    Deleted Gtzan code

--- pytorch/losses.py
+++ pytorch/losses.py
@@ -4,9 +4,7 @@ import torch.nn.functional as F
 
 def clip_nll(output_dict, target_dict):
     loss = F.binary_cross_entropy(torch.sigmoid(output_dict['clipwise_output']).to(torch.float32), target_dict['target'].to(torch.float32))
-    # loss = - torch.mean(target_dict['target'] * output_dict['clipwise_output'])
     return loss
-
 
 def get_loss_func(loss_type):
     if loss_type == 'clip_nll':
